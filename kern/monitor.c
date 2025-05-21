40c5215b333c9bf658c5909388b48be3c3b60c3e
leoh <zh3083@nyu.edu>
2025-05-20 23:11:13 -0400

    lab 2 add showmapping; decides  to not do boucning kernel yet

--- kern/monitor.c
+++ kern/monitor.c
@@ -10,6 +10,7 @@
 #include <kern/console.h>
 #include <kern/monitor.h>
 #include <kern/kdebug.h>
+#include <kern/pmap.h>
 
 #define CMDBUF_SIZE	80	// enough for one VGA text line
 
@@ -26,6 +27,7 @@ static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
 	{ "backtrace", "Display the backtrace of the current stack", mon_backtrace},
+	{ "showmapping", "Show virtual memory mapping of [va_start, va_end) of kernel (currently)", mon_showmapping},
 };
 
 /***** Implementations of basic kernel monitor commands *****/
@@ -68,7 +70,58 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 	return 0;
 }
 
+static void
+print_pt_perm(pte_t pe)
+{
+	cprintf("%c%c%c",
+			pe & PTE_PS ? 'M' : 'K',
+			pe & PTE_W ? 'w' : '-',
+			pe & PTE_U ? 'u' : '-'
+			);
+}
 
+int
+mon_showmapping(int argc, char **argv, struct Trapframe *tf)
+{
+	if (argc != 3) {
+		cprintf("usage:\n  showmapping va_start va_end (in hex)\n");
+		return 0;
+	}
+	uintptr_t va_start = strtol(argv[1], NULL, 16), va_end = strtol(argv[2], NULL, 16);
+	if (va_start % PGSIZE || va_end % PGSIZE || va_end <= va_start) {
+		cprintf("invalid va address\n");
+		return 0;
+	}
+	// NOTE a lot of bandaid code here, because this code section is copied from a test in pmap.c
+	extern pte_t *kern_pgdir;
+	for (uintptr_t addr = va_start; addr < va_end; addr += PGSIZE) {
+		cprintf("%p: ", addr);
+
+		pte_t *pgdir = kern_pgdir;
+		pte_t *p;
+		pgdir = &pgdir[PDX(addr)];
+		if (!(*pgdir & PTE_P)) {
+			cprintf("unmapped1\n");
+			continue;
+		}
+		if ((*pgdir & PTE_P) && (*pgdir & PTE_PS)) {
+			cprintf("%08p ", PTE_ADDR(*pgdir) + (PTX(addr) << PGSHIFT));
+			print_pt_perm(*pgdir);
+			cprintf("\n");
+			continue;
+		}
+		p = (pte_t*) KADDR(PTE_ADDR(*pgdir));
+		if (!(p[PTX(addr)] & PTE_P)) {
+			cprintf("unmapped2\n");
+			continue;
+		}
+		cprintf("%08p ", PTE_ADDR(p[PTX(addr)]));
+		print_pt_perm(p[PTX(addr)]);
+		cprintf("\n");
+	}
+
+	return 0;
+}
 
 /***** Kernel monitor command interpreter *****/
 
